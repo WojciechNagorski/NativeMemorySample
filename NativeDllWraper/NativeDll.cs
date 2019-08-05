4a796e4feb8138186294a0bf173af8bd5d021b27
Wojciech Nagórski <wojtpl2@gmail.com>
2019-08-05 23:14:25 +0200

    The initial version of the sample application

---
+++ NativeDllWraper/NativeDll.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NativeDllWraper
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Point
+    {
+        public int X;
+        public int Y;
+    }
+
+    public class NativeDll
+    {
+        [DllImport("NativeDll.dll")]
+        public static extern IntPtr AllocateArrayOfInt(int count);
+
+        [DllImport("NativeDll.dll")]
+        public static extern void DeallocateArrayOfInt(IntPtr ptr);
+
+
+        [DllImport("NativeDll.dll")]
+        public static extern IntPtr AllocateArrayOfPoint(int count);
+
+        [DllImport("NativeDll.dll")]
+        public static extern void DeallocateArrayOfPoint(IntPtr ptr);
+    }
+}
---
+++ NativeMemorySample/IntroNativeMemory.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+using NativeDllWraper;
+
+namespace NativeMemorySample
+{
+    [ShortRunJob]
+    [NativeMemoryProfiler]
+    [MemoryDiagnoser]
+    public class IntroNativeMemory
+    {
+        [Benchmark]
+        public void AllocManyType()
+        {
+            var ptr = NativeDll.AllocateArrayOfInt(200);
+            ptr = NativeDll.AllocateArrayOfInt(150);
+            ptr = NativeDll.AllocateArrayOfPoint(200);
+        }
+
+        [Benchmark]
+        public void AllocAndFreeNativeInt()
+        {
+            var ptr = NativeDll.AllocateArrayOfInt(200);
+            NativeDll.DeallocateArrayOfInt(ptr);
+        }
+
+        [Benchmark]
+        public void AllocNativeInt()
+        {
+            var ptr = NativeDll.AllocateArrayOfInt(200);
+        }
+
+        [Benchmark]
+        public void AllocAndFreeNativeStruct()
+        {
+            var ptr = NativeDll.AllocateArrayOfPoint(200);
+            NativeDll.DeallocateArrayOfPoint(ptr);
+        }
+
+        [Benchmark]
+        public void AllocNativeStruct()
+        {
+            var ptr = NativeDll.AllocateArrayOfPoint(200);
+        }
+    }
+}
