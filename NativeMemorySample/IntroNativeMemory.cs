2c163ab7cc74d7749bb4417a8b370cc5ab68cb42
Wojciech Nagórski <wojtpl2@gmail.com>
2020-05-07 22:45:55 +0200

    Add IntroStackTrace sample

--- NativeMemorySample/IntroNativeMemory.cs
+++ NativeMemorySample/IntroNativeMemory.cs
@@ -5,27 +5,29 @@ using BenchmarkDotNet.Diagnostics.Windows.Configs;
 
 namespace NativeMemorySample
 {
-[ShortRunJob]
-[NativeMemoryProfiler]
-[MemoryDiagnoser]
-public class IntroNativeMemory
-{
-    private const int Size = 20; // Greater value could cause System.OutOfMemoryException for test with memory leaks.
-    private int ArraySize = Size * Marshal.SizeOf(typeof(int));
-
-    [Benchmark]
-    public unsafe void AllocHGlobal()
+    [ShortRunJob]
+    [NativeMemoryProfiler]
+    [MemoryDiagnoser]
+    public class IntroNativeMemory
     {
-        IntPtr unmanagedHandle = Marshal.AllocHGlobal(ArraySize);
-        Span<byte> unmanaged = new Span<byte>(unmanagedHandle.ToPointer(), ArraySize);
-        Marshal.FreeHGlobal(unmanagedHandle);
-    }
+        private const int
+            Size = 20; // Greater value could cause System.OutOfMemoryException for test with memory leaks.
 
-    [Benchmark]
-    public unsafe void AllocHGlobalWithLeaks()
-    {
-        IntPtr unmanagedHandle = Marshal.AllocHGlobal(ArraySize);
-        Span<byte> unmanaged = new Span<byte>(unmanagedHandle.ToPointer(), ArraySize);
+        private int ArraySize = Size * Marshal.SizeOf(typeof(int));
+
+        [Benchmark]
+        public unsafe void AllocHGlobal()
+        {
+            IntPtr unmanagedHandle = Marshal.AllocHGlobal(ArraySize);
+            Span<byte> unmanaged = new Span<byte>(unmanagedHandle.ToPointer(), ArraySize);
+            Marshal.FreeHGlobal(unmanagedHandle);
+        }
+
+        [Benchmark]
+        public unsafe void AllocHGlobalWithLeaks()
+        {
+            IntPtr unmanagedHandle = Marshal.AllocHGlobal(ArraySize);
+            Span<byte> unmanaged = new Span<byte>(unmanagedHandle.ToPointer(), ArraySize);
+        }
     }
-}
 }
---
+++ NativeMemorySample/IntroStackTrace.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.Windows.Configs;
+
+namespace NativeMemorySample
+{
+    [ShortRunJob]
+    [NativeMemoryProfiler]
+    [MemoryDiagnoser]
+    public class IntroStackTrace
+    {
+
+        [Benchmark]
+        public string StackTrace()
+        {
+            var stackTrace = new StackTrace();
+
+            return stackTrace.GetType().Name;
+        }
+    }
+}
